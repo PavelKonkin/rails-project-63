a28fa9bfa3b8903d17eb047a2722c170a86101bd
Pavel Konkin <idmfani@gmail.com>
2023-03-12 14:07:39 +0300

    Add test for form generation

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -13,6 +13,14 @@ class TestHexletCode < Minitest::Test
     assert { form_for("user") == "<form action=\"#\" method=\"post\"></form>" }
   end
 
+  def test_should_return_tag_with_address
+    assert { form_for "user", url: "/users" == "<form action=\"/users\" method=\"post\"></form>" }
+  end
+
+  def test_should_return_tag_whith_default_action
+    assert { form_for "user" == "<form action=\"#\" method=\"post\"></form>" }
+  end
+
   def test_it_does_something_useful
     assert { 5.class.instance_of? [].class }
   end
