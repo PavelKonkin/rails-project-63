2be11aa95e6353278400aefe77ca9741125aee0b
Pavel Konkin <idmfani@gmail.com>
2023-03-12 13:58:03 +0300

    Make corrections for lint to pass

--- Gemfile
+++ Gemfile
@@ -8,8 +8,7 @@ gemspec
 gem "rake", "~> 13.0"
 
 gem "minitest", "~> 5.0"
-
+gem "minitest-power_assert"
 gem "rubocop", "~> 1.21", require: false
 gem "rubocop-minitest"
 gem "rubocop-rake"
-gem "minitest-power_assert"
--- lib/tag.rb
+++ lib/tag.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 #  Module for tag generation
 module Tag
-    def build(tag, **args)
-      single_tags = %w[br image input]
-      double_tags = %w[label div form]
-      attr = ""
-      args.each do |key, value|
-        attr = "#{attr} #{key}=\"#{value}\""
-      end
-      body = (yield if block_given?).to_s
-      return "<#{tag}#{attr}>" if single_tags.include? tag
-
-      return "<#{tag}#{attr}>#{body}</#{tag}>" if double_tags.include? tag
+  def build(tag, **args)
+    single_tags = %w[br image input]
+    double_tags = %w[label div form]
+    attr = ""
+    args.each do |key, value|
+      attr = "#{attr} #{key}=\"#{value}\""
     end
+    body = (yield if block_given?).to_s
+    return "<#{tag}#{attr}>" if single_tags.include? tag
+
+    return "<#{tag}#{attr}>#{body}</#{tag}>" if double_tags.include? tag
+  end
 end
