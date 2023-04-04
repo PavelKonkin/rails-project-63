51dfba9c9972ff41ecd5279d16e9e24ec9bddbc5
Pavel Konkin <idmfani@gmail.com>
2023-04-04 10:03:51 +0300

    Remove 'get' from methods names and merge! method from form_tag method

--- lib/hexlet_code/form.rb
+++ lib/hexlet_code/form.rb
@@ -6,30 +6,33 @@ module HexletCode
     attr_accessor :body
 
     def initialize(form_structure)
-      tag_arr = get_tag_arr(form_structure)
+      tag_arr = tag_arr(form_structure)
       @body = tag_arr.join
     end
 
     private
 
-    def get_tag_arr(form_structure)
-      tag_arr = [get_form_tag(form_structure)]
+    def tag_arr(form_structure)
+      tag_arr = [form_tag(form_structure)]
       form_structure.scheme[:tag_list].each do |tag|
         tag_arr << "  #{Tag.build(tag.scheme[:tag_type], tag.scheme)}\n"
       end
-      tag_arr.append(get_button_tag(form_structure)) if form_structure.scheme.key? :button
+      tag_arr.append(button_tag(form_structure)) if form_structure.scheme.key? :button
       tag_arr << "</form>\n"
     end
 
-    def get_form_tag(form_structure)
-      form_options = { action: form_structure.scheme[:form][:url] }
-      form_options.merge! form_structure.scheme[:form].except :url
+    def form_tag(form_structure)
+      form_options = form_options(form_structure)
       "#{Tag.build('form', form_options)}\n"
     end
 
-    def get_button_tag(form_structure)
+    def button_tag(form_structure)
       button_scheme = form_structure.scheme[:button].scheme
       "  #{Tag.build('input', { type: 'submit', value: button_scheme[:value] })}\n"
+    end
+
+    def form_options(form_structure)
+      { action: form_structure.scheme[:form][:url] }.merge form_structure.scheme[:form].except :url
     end
   end
 end
